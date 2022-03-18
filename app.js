362876de84ca5aff52e99d1cd44241fe1ea016bd
orhanurullah <orhann.1512@gmail.com>
2022-03-18 22:42:55 +0300

    static files setter

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const fn = () => {
-    console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const myLogger = (req,res,next) => {
+    console.log("Middleware log 1");
+    next();
+}
+
+//MIDDLEWARE
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`PCAT is starting on ${port}`);
+});
